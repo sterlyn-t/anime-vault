0c3aa8fc2879f6a2f71081efdddcd03c9c216575
Sterlyn <sterlyntang31@gmail.com>
2024-03-28 15:10:18 -0400

    Feat: Scroll to current episode

--- app/anime/[slug]/[episode]/EpisodesList.tsx
+++ app/anime/[slug]/[episode]/EpisodesList.tsx
@@ -1,9 +1,10 @@
 "use client";
+import { Button } from "@/components/ui/button";
 import { ScrollArea, ScrollViewport } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
-import { AudioLines, Eye } from "lucide-react";
+import { ArrowDown, AudioLines } from "lucide-react";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 interface EpisodesListProps {
   episodes: number;
@@ -16,16 +17,34 @@ const EpisodesList: React.FC<EpisodesListProps> = ({
   currentEpisodeIndex,
   slug,
 }) => {
-  if (!episodes) {
-    return null;
-  }
+  const currentEpisodeRef = useRef<HTMLDivElement>(null);
+  const episodesListRef = useRef<HTMLDivElement>(null);
+
+  const scrollToCurrentEpisode = () => {
+    if (episodesListRef.current && currentEpisodeRef.current) {
+      currentEpisodeRef.current.scrollIntoView({
+        behavior: "smooth",
+        block: "center",
+      });
+    }
+  };
+
+  useEffect(() => {
+    scrollToCurrentEpisode();
+  }, []);
   return (
     <>
-      <h1 className="text-white font-semibold text-xl tracking-tight mb-2 mt-2">
-        Episodes
-      </h1>
+      <div className="flex flex-row mb-2 mt-2 items-center justify-between">
+        <h1 className="text-white font-semibold text-xl tracking-tight">
+          Episodes
+        </h1>
+        <Button size="sm" onClick={scrollToCurrentEpisode}>
+          <ArrowDown height={16} width={16} />
+          <p className="text-xs">Jump to current</p>
+        </Button>
+      </div>
       <ScrollArea className="h-[10rem] w-full rounded-md border-slate-800 border lg:h-[32rem] xl:h-[33rem]">
-        <ScrollViewport className="p-4">
+        <ScrollViewport className="p-4" ref={episodesListRef}>
           {Array.from({ length: episodes }, (_, index) => index + 1).map(
             (_, index) => (
               <>
@@ -34,9 +53,11 @@ const EpisodesList: React.FC<EpisodesListProps> = ({
                     <Link href={`/anime/${slug}/${index}`} key={index}>
                       <div className="flex py-4">
                         {index === currentEpisodeIndex ? (
-                          <div className="flex items-center justify-between">
+                          <div
+                            className="flex items-center justify-between"
+                            ref={currentEpisodeRef}
+                          >
                             <p className=" text-green-700">Episode {index}</p>
-
                             <AudioLines
                               className="text-green-700 animate-pulse"
                               height={16}

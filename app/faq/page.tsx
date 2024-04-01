f2d88e56531272c9b788dedbcf311c2aafb6c708
Sterlyn <sterlyntang31@gmail.com>
2024-04-01 16:27:34 -0400

    Feat: added faq

---
+++ app/faq/page.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+type FAQ = {
+  question: string;
+  answer: string;
+};
+
+const faqItems: FAQ[] = [
+  {
+    question: "Is AnimeVault completely free?",
+    answer: "Yes! (and will always be)",
+  },
+  {
+    question: "Does AnimeVault own/store any of the content?",
+    answer:
+      "Nope, AnimeVault simply connects to servers such as gogoanime to provide epsiode streams.",
+  },
+  {
+    question: "Is there a way to track watch history?",
+    answer: "Not right now but it is in the works!",
+  },
+];
+
+const Page = () => {
+  return (
+    <div className="min-h-[80vh] py-8 xl:px-24 px-8 flex flex-col gap-8 items-center">
+      <h1 className="text-white font-bold text-2xl">
+        Frequently Asked Questions
+      </h1>
+      <Accordion type="single" collapsible className="w-full">
+        {faqItems.map((value, index) => (
+          <AccordionItem value={index.toString()} key={index}>
+            <AccordionTrigger className="text-white">
+              {value.question}
+            </AccordionTrigger>
+            <AccordionContent className="text-zinc-300">
+              {value.answer}
+            </AccordionContent>
+          </AccordionItem>
+        ))}
+      </Accordion>
+    </div>
+  );
+};
+
+export default Page;
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 function Footer() {
   return (
-    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921] border-t border-zinc-800 min-w-full">
+    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921] border-t border-zinc-800 w-full">
       <p className="text-base font-bold text-white">@2024 AnimeVault</p>
       <Image
         src="/logo.svg"
@@ -14,9 +14,11 @@ function Footer() {
         className="object-contain"
       />
       <div className="flex items-center gap-6">
-        <p className=" text-white text-sm hover:underline cursor-pointer">
-          FAQ
-        </p>
+        <Link href={"/faq"}>
+          <p className=" text-white text-sm hover:underline cursor-pointer">
+            FAQ
+          </p>
+        </Link>
         <Link href={"https://github.com/sterlyn-t/anime-vault"} target="_blank">
           <Github width={19} height={19} color="white" />
         </Link>
